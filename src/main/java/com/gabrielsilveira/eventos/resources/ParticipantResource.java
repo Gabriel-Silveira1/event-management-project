e4b9ac266a32e60b841b17ef6d21bdfcccb4c70d
Gabriel Silveira <gabriel-silveira1@hotmail.com>
2026-06-22 15:13:18 -0300

    ParticipantResource created

---
+++ src/main/java/com/gabrielsilveira/eventos/resources/ParticipantResource.java
@@ -0,0 +1,22 @@
+package com.gabrielsilveira.eventos.resources;
+
+import com.gabrielsilveira.eventos.entities.Participant;
+import com.gabrielsilveira.eventos.services.ParticipantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/participants")
+public class ParticipantResource {
+    @Autowired
+    ParticipantService participantService;
+
+    public ResponseEntity<List<Participant>> findAll() {
+        List<Participant> participants = participantService.findAll();
+        return ResponseEntity.ok().body(participants);
+    }
+}
