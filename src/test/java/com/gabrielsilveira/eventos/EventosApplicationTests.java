edd5bf1a5490ff67c2272dfd2d0a5361bdedfcd1
Gabriel Silveira <gabriel-silveira1@hotmail.com>
2026-06-21 21:25:17 -0300

    Project created

---
+++ src/test/java/com/gabrielsilveira/eventos/EventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielsilveira.eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
