e7a3005c390b604febcb494f5c2c6f62dff926a2
Gabriel Silveira <gabriel-silveira1@hotmail.com>
2026-06-23 16:05:42 -0300

    Category seed added

--- src/main/java/com/gabrielsilveira/eventos/config/TestConfig.java
+++ src/main/java/com/gabrielsilveira/eventos/config/TestConfig.java
@@ -1,6 +1,8 @@
 package com.gabrielsilveira.eventos.config;
 
+import com.gabrielsilveira.eventos.entities.Category;
 import com.gabrielsilveira.eventos.entities.Participant;
+import com.gabrielsilveira.eventos.repositories.CategoryRepository;
 import com.gabrielsilveira.eventos.repositories.ParticipantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -15,10 +17,19 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     ParticipantRepository participantRepository;
 
+    @Autowired
+    CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
         Participant par1 = new Participant(null, "Lucas Ferreira", "lucas@gmail.com", "11988880001", "CPF-111.222.333-44");
         Participant par2 = new Participant(null, "Camila Rocha", "camila@gmail.com", "11988880002", "CPF-555.666.777-88");
         participantRepository.saveAll(Arrays.asList(par1, par2));
+
+        Category cat1 = new Category(null, "Technology");
+        Category cat2 = new Category(null, "Music");
+        Category cat3 = new Category(null, "Sports");
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
     }
 }
--- src/main/java/com/gabrielsilveira/eventos/resources/CategoryResource.java
+++ src/main/java/com/gabrielsilveira/eventos/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import com.gabrielsilveira.eventos.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,6 +16,7 @@ public class CategoryResource {
     @Autowired
     CategoryService categoryService;
 
+    @GetMapping
     public ResponseEntity<List<Category>> findAll() {
         List<Category> categories = categoryService.findAll();
         return ResponseEntity.ok().body(categories);
