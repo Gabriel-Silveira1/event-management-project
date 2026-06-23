92ce8c3be32924ae3b7412a5b3f7e509867361a8
Gabriel Silveira <gabriel-silveira1@hotmail.com>
2026-06-23 15:57:48 -0300

    CategoryResource created

---
+++ src/main/java/com/gabrielsilveira/eventos/resources/CategoryResource.java
@@ -0,0 +1,22 @@
+package com.gabrielsilveira.eventos.resources;
+
+import com.gabrielsilveira.eventos.entities.Category;
+import com.gabrielsilveira.eventos.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+    @Autowired
+    CategoryService categoryService;
+
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> categories = categoryService.findAll();
+        return ResponseEntity.ok().body(categories);
+    }
+}
