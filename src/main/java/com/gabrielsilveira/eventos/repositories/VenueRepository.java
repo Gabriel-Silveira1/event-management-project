67dd21d60effc7c95eb08bb3be5727899309ae8b
Gabriel Silveira <gabriel-silveira1@hotmail.com>
2026-06-24 18:05:54 -0300

    VenueService created

--- src/main/java/com/gabrielsilveira/eventos/repositories/VenueRepository.java
+++ src/main/java/com/gabrielsilveira/eventos/repositories/VenueRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VenueRepository extends JpaRepository<Venue, Long> {
-    
+
 }
---
+++ src/main/java/com/gabrielsilveira/eventos/services/VenueService.java
@@ -0,0 +1,18 @@
+package com.gabrielsilveira.eventos.services;
+
+import com.gabrielsilveira.eventos.entities.Venue;
+import com.gabrielsilveira.eventos.repositories.VenueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VenueService {
+    @Autowired
+    VenueRepository venueRepository;
+
+    public List<Venue> findAll() {
+        return venueRepository.findAll();
+    }
+}
