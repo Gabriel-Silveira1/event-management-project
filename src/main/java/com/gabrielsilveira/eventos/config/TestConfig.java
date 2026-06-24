56209d5a8b24ace4b69f8a7b6dbf725b615ccdc1
Gabriel Silveira <gabriel-silveira1@hotmail.com>
2026-06-24 18:13:17 -0300

    Venue seed added

--- src/main/java/com/gabrielsilveira/eventos/config/TestConfig.java
+++ src/main/java/com/gabrielsilveira/eventos/config/TestConfig.java
@@ -2,8 +2,10 @@ package com.gabrielsilveira.eventos.config;
 
 import com.gabrielsilveira.eventos.entities.Category;
 import com.gabrielsilveira.eventos.entities.Participant;
+import com.gabrielsilveira.eventos.entities.Venue;
 import com.gabrielsilveira.eventos.repositories.CategoryRepository;
 import com.gabrielsilveira.eventos.repositories.ParticipantRepository;
+import com.gabrielsilveira.eventos.repositories.VenueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +22,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     CategoryRepository categoryRepository;
 
+    @Autowired
+    VenueRepository venueRepository;
+
     @Override
     public void run(String... args) throws Exception {
         Participant par1 = new Participant(null, "Lucas Ferreira", "lucas@gmail.com", "11988880001", "CPF-111.222.333-44");
@@ -31,5 +36,8 @@ public class TestConfig implements CommandLineRunner {
         Category cat3 = new Category(null, "Sports");
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
+        Venue v1 = new Venue(null, "Centro de Convenções SP", "Av. Paulista, 1000", 500);
+        Venue v2 = new Venue(null, "Arena Rio", "Rua das Laranjeiras, 200", 2000);
+        venueRepository.saveAll(Arrays.asList(v1, v2));
     }
 }
